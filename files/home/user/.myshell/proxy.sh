7aebcddff83aea7bf99c06b74f580b73630b21e5
root <root@x.com>
2023-03-28 17:19:06 +0800

    update

--- files/home/user/.myshell/proxy.sh
+++ files/home/user/.myshell/proxy.sh
@@ -1,7 +1,5 @@
+
 #/bin/bash
-###
- # @Date: 2023-03-26
- # @Description: 
- # @LastEditTime: 2023-03-26
- # @LastEditors: xushuwei
-### 
+alias xopenproxy="export http_proxy=http://192.168.1.199:10880;export https_proxy=http://192.168.1.199:10880;export no_proxy=\"*.gitclone.com,*.gitee.com,127.0.0.1, localhost, 192.168.*,10.*;\";echo \"HTTP Proxy on\";"
+alias xcloseproxy="export http_proxy=;export https_proxy=;echo \"HTTP Proxy off\";"
+    
