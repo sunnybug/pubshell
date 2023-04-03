bf348df21c1bc89dc13bdcd0e176c626384bc29c
xushuwei <x@x.com>
2023-04-03 17:07:30 +0800

    remove

--- files/home/user/.myshell/proxy.sh
+++
@@ -1,5 +0,0 @@
-
-#/bin/bash
-alias xopenproxy="export http_proxy=http://192.168.1.199:10880;export https_proxy=http://192.168.1.199:10880;export no_proxy=\"*.gitclone.com,*.gitee.com,127.0.0.1, localhost, 192.168.*,10.*;\";echo \"HTTP Proxy on\";"
-alias xcloseproxy="export http_proxy=;export https_proxy=;echo \"HTTP Proxy off\";"
-    
