5e0857287d93f1037776bef8f97831c34f4a78b1
root <root@x.com>
2023-05-05 17:37:03 +0800

    auto-commit

--- install-gitlab.sh
+++ install-gitlab.sh
@@ -1,55 +1,76 @@
 #!/bin/bash
 
+# ./install-gitlab.sh -u ip
+# 访问http://ip:40080，用户名root，密码见本脚本的输出
+
 while getopts ":u:" opt
 do
     case $opt in
         u)
-        echo "url:$OPTARG"
-        SERVER_URL=$OPTARG
+            echo "url:$OPTARG"
+            SERVER_URL=$OPTARG
         ;;
         ?)
-        echo "未知参数"
+            echo "未知参数"
         exit 1;;
     esac
 done
 
-export GITLAB_HOME=/data/gitlab
+if [ -z "$SERVER_URL" ]
+then
+    echo "未提供-u选项参数"
+    exit 1
+fi
+
+
 
 if [ "$EUID" -ne 0 ]; then
-  echo "请使用root用户执行此脚本"
-  exit
+    echo "请使用root用户执行此脚本"
+    exit
 fi
 
 if which docker >/dev/null; then
-  echo "Docker is installed"
-  echo "SERVER_URL:${SERVER_URL}"
-  echo "GITLAB_HOME:${GITLAB_HOME}"
+    echo "Docker is installed"
+    echo "SERVER_URL:${SERVER_URL}"
 else
-  echo "Docker is not installed"
-  exit 1;
+    echo "Docker is not installed"
+    exit 1;
 fi
 
-useradd docker-gitlab-user -u 1500 --shell /usr/sbin/nologin
-
-mkdir -p $GITLAB_HOME/config
-mkdir -p $GITLAB_HOME/logs
-mkdir -p $GITLAB_HOME/data
-
-chown -R docker-gitlab-user:docker-gitlab-user /data/gitlab
-
+mkdir -p /data/gitlab/config
+mkdir -p /data/gitlab/logs
+mkdir -p /data/gitlab/data
 docker pull gitlab/gitlab-ee:latest
+SERVER_URL="${SERVER_URL}:40080"
+
 docker run --detach \
-  --user 1500 \
-  --hostname $SERVER_URL \
-  --publish 10443:443 --publish 10080:80 --publish 10022:22 --publish 15050:5050 \
-  --name gitlab \
-  --restart always \
-  --volume $GITLAB_HOME/config:/etc/gitlab \
-  --volume $GITLAB_HOME/logs:/var/log/gitlab \
-  --volume $GITLAB_HOME/data:/var/opt/gitlab \
-  --shm-size 256m \
-  gitlab/gitlab-ee:latest
+--hostname $SERVER_URL \
+--env GITLAB_OMNIBUS_CONFIG="external_url 'http://${SERVER_URL}';registry_external_url 'http://${SERVER_URL}' " \
+--publish 40443:443 --publish 40080:80 --publish 40022:22 --publish 45050:5050 \
+--name gitlab \
+--restart always \
+--volume /data/gitlab/config:/etc/gitlab \
+--volume /data/gitlab/logs:/var/log/gitlab \
+--volume /data/gitlab/data:/var/opt/gitlab \
+--shm-size 256m \
+gitlab/gitlab-ee:latest
 
-sudo sed -i "$ a registry_external_url \"${SERVER_URL}:5050\"" /data/gitlab/config/gitlab.rb
+url="http://${SERVER_URL}/users/sign_in"
+echo "url=$url"
 
-docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password
+while true
+do
+    status=$(curl -s -o /dev/null -w '%{http_code}' "$url")
+    
+    if [ "$status" -eq 200 ]; then
+        echo "The server is up and running."
+        echo "User:root"
+        echo "Password:"
+        docker exec -it gitlab grep 'Password:' /etc/gitlab/initial_root_password
+        break
+    else
+        wait_time=3
+        echo "The server is not yet ready. Waiting for $wait_time seconds..."
+        sleep $wait_time
+    fi
+done
