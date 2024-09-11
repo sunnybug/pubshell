409e40b4c6a711b602f5c43868f5bd3ccd90dad1
root <root@x.com>
2024-09-11 16:54:25 +0800

    docker-rootless

--- tools/docker-rootless/docker-rootless-user.sh
+++ tools/docker-rootless/docker-rootless-user.sh
@@ -3,7 +3,7 @@ set -e
 
 # 不能是root
 if [ "$(id -u)" = "0" ]; then
-    echo "please run as non-root user"
+    echo "不允许root执行"
     exit 1
 fi
 
@@ -32,20 +32,41 @@ fi
 curl -fsSL https://get.docker.com/rootless | FORCE_ROOTLESS_INSTALL=1 sh
 systemctl --user --now enable docker
 
-echo "允许当前用户服务开机自启:y/n"
-read -r user_input
-if [[ "$user_input" == "y" ]]; then
-    echo "sudo loginctl enable-linger $USER"
-    sudo loginctl enable-linger $USER
-    echo "允许当前用户服务开机自启"
+####################
+#开机自启
+if sudo loginctl show-user "$USER" | grep -qw 'Linger=yes'; then
+    echo "当前用户服务已经设置为开机自启"
 else
-    echo '禁止当前用户服务开机自启'
+    echo "是否允许当前用户服务开机自启:y/n"
+    read -r user_input
+    if [[ "$user_input" == "y" ]]; then
+        echo "sudo loginctl enable-linger $USER"
+        if sudo loginctl enable-linger "$USER"; then
+            echo "成功设置开机自启。"
+        else
+            echo "无法设置开机自启，请检查权限或联系管理员。"
+        fi
+    else
+        echo '禁止当前用户服务开机自启'
+    fi
 fi
 
 ###############
-#手动处理
-cat <<EOF
-you should add to ~/.bashrc:
-export PATH=\~/bin:\$PATH
-export DOCKER_HOST=unix:///run/user/1000/docker.sock
-EOF
+# 检查PATH是否已经包含~/bin
+current_path="$PATH"
+if [[ "$current_path" != *":$HOME/bin:"* ]]; then
+    echo 'export PATH="$HOME/bin:$PATH"' >> ~/.bashrc
+    echo "Added '~/bin' to PATH in ~/.bashrc"
+else
+    echo " '~/bin' is already in PATH"
+fi
+
+###############
+# 检查DOCKER_HOST环境变量是否已设置
+if [ -z "$DOCKER_HOST" ]; then
+    # 环境变量未设置，将其添加到~/.bashrc
+    echo 'export DOCKER_HOST=unix://run/user/1000/docker.sock' >> ~/.bashrc
+    echo "DOCKER_HOST set to 'unix://run/user/1000/docker.sock' in ~/.bashrc"
+else
+    echo "DOCKER_HOST is already set to '$DOCKER_HOST'"
+fi
