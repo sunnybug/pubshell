a99dc72a0b4d90b345d16c3c363736ec5ef43383
xushuwei <xushuwei@x.com>
2024-09-26 16:24:06 +0800

    优化

--- files/home/user/.myshell/tools/docker_mirror.sh
+++ files/home/user/.myshell/tools/docker_mirror.sh
@@ -29,6 +29,11 @@ docker_rootless_proxy(){
         mkdir -p ~/.config/systemd/user/docker.service.d
         proxy_conf=~/.config/systemd/user/docker.service.d/proxy.conf
         if [ -f $proxy_conf ]; then
+            # 如果旧文件中已存在192.168.1.199，则跳过
+            if grep -q "192.168.1.199" $proxy_conf; then
+                echo "proxy_conf already exists 192.168.1.199, skip"
+                return
+            fi
             mv $proxy_conf ${proxy_conf}.bak.$(date +"%Y%m%d%H%M%S")
         fi
 cat <<EOF > $proxy_conf
@@ -37,9 +42,9 @@ Environment="HTTP_PROXY=http://192.168.1.199:10816/"
 Environment="HTTPS_PROXY=http://192.168.1.199:10816/"
 Environment="NO_PROXY=*.aliyuncs.com,*.tencentyun.com,*.cn,*.zentao.net"
 EOF
-    fi
     echo "create suc: $proxy_conf"
     echo '手动执行: systemctl --user daemon-reload && systemctl --user restart docker'
+    fi
 }
 
 docker_rootless_mirror(){
@@ -48,6 +53,11 @@ docker_rootless_mirror(){
 }
 
 auto_docker_mirror(){
+    # 检查是否存在docker命令
+    if ! [ -x "$(command -v docker)" ]; then
+        return
+    fi
+
     # 如果docker info返回中包含rootless
     if docker info | grep -q "rootless"; then
         docker_rootless_proxy
