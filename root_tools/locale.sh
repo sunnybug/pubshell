e8b17dcd54460b209de6d5d546ee096412fc6870
xushuwei <xushuwei@x.com>
2024-09-27 23:58:48 +0800

    locale.sh

--- root_tools/locale.sh
+++ root_tools/locale.sh
@@ -1,8 +1,11 @@
 # export LANG=en_US.UTF-8
 # 检查 /etc/environment 中是否存在 "LANG=en_US.UTF-8"
-if ! grep -q "LANG=en_US.UTF-8" /etc/environment; then
-    echo "LANG=en_US.UTF-8" >> /etc/environment
+if ! grep -q "LANG=en_GB.UTF-8" /etc/environment; then
+    echo "LANG=en_GB.UTF-8" >> /etc/environment
+    echo "LANGUAGE=en_GB.UTF-8" >> /etc/environment
+    echo "LC_ALL=en_GB.UTF-8" >> /etc/environment
 fi
 source /etc/environment
 apt install locales  locales-all -y
 locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8
+
