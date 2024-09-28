6f5dc9664b18a6c2476c981998e46a16fc57fbe3
xushuwei <xushuwei@x.com>
2024-09-28 16:04:55 +0800

    x

--- root_tools/locale.sh
+++ root_tools/locale.sh
@@ -1,11 +1,22 @@
+#!/bin/bash
+set -e
+
 # export LANG=en_US.UTF-8
-# 检查 /etc/environment 中是否存在 "LANG=en_US.UTF-8"
-if ! grep -q "LANG=en_GB.UTF-8" /etc/environment; then
-    echo "LANG=en_GB.UTF-8" >> /etc/environment
-    echo "LANGUAGE=en_GB.UTF-8" >> /etc/environment
-    echo "LC_ALL=en_GB.UTF-8" >> /etc/environment
+# 检查 /etc/profile 中是否存在 "LANG=en_US.UTF-8"
+# if ! grep -q "LANG=en_US.UTF-8" /etc/profile; then
+#     echo "LANG=en_US.UTF-8" >> /etc/profile
+#     echo "LANGUAGE=en_US.UTF-8" >> /etc/profile
+#     echo "LC_ALL=en_US.UTF-8" >> /etc/profile
+# fi
+
+# source /etc/profile
+
+# 检查$LANG环境变量是否定义
+if [ -z "$LANG" ]; then
+    echo "错误：\$LANG 未定义。请设置 \$LANG 环境变量。"
+    exit 1
 fi
-source /etc/environment
 apt install locales  locales-all -y
-locale-gen en_US.UTF-8 && update-locale LANG=en_US.UTF-8
+
+locale-gen $LANG && update-locale LANG=$LANG
 
