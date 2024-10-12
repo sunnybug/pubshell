06a9386cc4ac745e9687c935d6f702ce7b7760a8
xushuwei <xushuwei@x.com>
2024-10-12 19:09:10 +0800

    x

--- root_tool/install_docker.sh
+++ root_tool/install_docker.sh
@@ -4,13 +4,12 @@ function install_docker(){
     echo "[...]install_docker......."
     sudo apt update
     sudo apt install --no-install-recommends -y gpg curl
-    result=$(curl -IsL https://download.docker.com/linux/debian/gpg --connect-timeout 2 --max-time 2)
-    # 如果result中不含KEY则认为失败
-    if [[ $result != *"KEY"* ]]; then
-        echo "可能需要开代理，访问失败:https://download.docker.com/linux/debian/gpg"
-        return 1
+    curl -sS https://download.docker.com/linux/debian/gpg --connect-timeout 2 --max-time 2 | gpg --dearmor > /usr/share/keyrings/docker-ce.gpg
+    if [ $? -ne 0 ]; then
+        echo "gpg error"
+        return
     fi
-    
+
     echo $result | gpg --dearmor > /usr/share/keyrings/docker-ce.gpg
     source /etc/os-release
     sudo echo "deb [arch=$(dpkg --print-architecture) signed-by=/usr/share/keyrings/docker-ce.gpg] https://mirrors.tuna.tsinghua.edu.cn/docker-ce/linux/debian $VERSION_CODENAME stable" > /etc/apt/sources.list.d/docker.list
