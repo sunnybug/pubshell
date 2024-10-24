e56fc1df529074ae3f27912b39793a20dfb0c24d
xushuwei <xushuwei@x.com>
2024-10-24 16:57:18 +0800

    x

--- root_tool/install_ssh.sh
+++ root_tool/install_ssh.sh
@@ -6,19 +6,14 @@
 # sudo apt update
 # sudo apt install -y git zsh curl lua5.4 jq
 
-sudo apt install -y openssh-client openssh-server
+sudo apt install -y openssh-server
 
-sudo cp /etc/ssh/sshd_config /etc/ssh/sshd_config.bak
 sudo sed -i 's/^#Port /Port /' /etc/ssh/sshd_config
 sudo sed -i 's/^Port .*/Port 2222/' /etc/ssh/sshd_config
 sudo sed -i 's/^#ListenAddress 0.0.0.0/ListenAddress 0.0.0.0/' /etc/ssh/sshd_config
-sudo systemctl enable ssh
-sudo systemctl restart ssh
-
-# 如果是wsl，则需要重启
-if [ -f /etc/wsl.conf ]; then
-    echo "wsl下如果ssh端口没修改成功，可能需要重启wsl --shutdown"
-fi
+sudo systemctl restart ssh ssh.socket
 
 # 在宿主机转发端口
 # netsh interface portproxy add v4tov4 listenport=2223 listenaddress=0.0.0.0 connectport=2222 connectaddress=172.25.13.184
+# netsh interface portproxy add v6tov6 listenport=2223 listenaddress=[::] connectport=2222 connectaddress=[::1]
+# netsh interface portproxy delete v6tov6 listenport=2223 listenaddress=[::]
--- root_tool/replace_source_list.sh
+++ root_tool/replace_source_list.sh
@@ -6,17 +6,28 @@ function _replace_debian_sources() {
     sudo sed -i "s/security.debian.org/mirrors.aliyun.com/g" /etc/apt/sources.list
     # 注释ftp.debian.org这一行
     sudo sed -i '/ftp.debian.org/s/^/#/' /etc/apt/sources.list
+
+    cat /etc/apt/sources.list
 }
 
 function _replace_ubuntu_sources() {
+    local UBUNTU_CODENAME=$(lsb_release -cs)
+
     sudo tee /etc/apt/sources.list.d/ubuntu.sources > /dev/null <<EOF
 Types: deb
 URIs: http://mirrors.163.com/ubuntu/
-Suites: noble noble-updates noble-security
+Suites: $UBUNTU_CODENAME $UBUNTU_CODENAME-updates $UBUNTU_CODENAME-security
 Components: main restricted universe multiverse
 Signed-By: /usr/share/keyrings/ubuntu-archive-keyring.gpg
 EOF
+    cat /etc/apt/sources.list.d/ubuntu.sources
 }
+
+# 必须是root
+if [ "$(id -u)" != "0" ]; then
+    echo "Please run as root"
+    exit 1
+fi
 
 if [ -x /usr/bin/apt ] && grep -q "NAME.*Debian" /etc/os-release; then
     echo "Current OS is Debian"
@@ -25,3 +36,5 @@ elif [ -x /usr/bin/apt ] && grep -q "NAME.*Ubuntu" /etc/os-release; then
     echo "Current OS is Ubuntu"
     _replace_ubuntu_sources
 fi
+
+sudo apt update
