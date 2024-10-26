6c45663662a977e6a2f2d621a8ecd3cb11ae8e40
xushuwei <xushuwei@x.com>
2024-10-26 21:58:51 +0800

    x

--- root_tool/docker-rootless/docker-rootless-user.sh
+++ root_tool/docker-rootless/docker-rootless-user.sh
@@ -51,4 +51,12 @@ fi
 
 curl -sSfL https://gitee.com/sunnybug/pubshell/raw/main/tool/config_docker.sh | bash
 
+#################################
+# docker compose plugin
+DOCKER_CONFIG=${DOCKER_CONFIG:-$HOME/.docker}
+mkdir -p $DOCKER_CONFIG/cli-plugins
+curl -SL https://github.com/docker/compose/releases/download/v2.29.6/docker-compose-linux-x86_64 -o $DOCKER_CONFIG/cli-plugins/docker-compose
+chmod +x $DOCKER_CONFIG/cli-plugins/docker-compose
+
+#################################
 echo "[SUC] Docker Rootless 安装完成,请重新登录"
