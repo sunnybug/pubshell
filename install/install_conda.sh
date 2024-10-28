20e50928c9d1075d4a728fac36f8f8b77f966d43
xushuwei <xushuwei@x.com>
2024-10-28 09:42:37 +0000

    x

--- install/install_conda.sh
+++ install/install_conda.sh
@@ -4,19 +4,21 @@
 setup_conda_environment() {
     # 设置conda镜像
     local CONDA_MIRROR="https://mirrors.tuna.tsinghua.edu.cn/anaconda"
-    local CONDA_SRC="https://repo.anaconda.com/miniconda"
-
-    # 创建临时目录和文件
     local TEMP_DIR=$(mktemp -d)
-    local MINICONDA_SH="${TEMP_DIR}/miniconda.sh"
 
-    # 下载Miniconda安装脚本到临时文件
-    local CONDA_INSTALLER_URL="${CONDA_SRC}/Miniconda3-latest-Linux-x86_64.sh"
-    curl -fsSL -v -o "${MINICONDA_SH}" "${CONDA_INSTALLER_URL}"
+    # install miniconda
+    if ! [ -d "${HOME}/miniconda" ]; then
+        # 创建临时目录和文件
+        local MINICONDA_SH="${TEMP_DIR}/miniconda.sh"
 
-    # 给安装脚本添加执行权限并执行安装
-    chmod +x "${MINICONDA_SH}"
-    "${MINICONDA_SH}" -b -p "${HOME}/miniconda"
+        echo "[...]下载Miniconda安装脚本到临时文件"
+        curl -fsSL -o "${MINICONDA_SH}" "https://repo.anaconda.com/miniconda/Miniconda3-latest-Linux-x86_64.sh"
+
+        # 给安装脚本添加执行权限并执行安装
+        chmod +x "${MINICONDA_SH}"
+        echo "[...]Installing Miniconda..."
+        "${MINICONDA_SH}" -b -p "${HOME}/miniconda"
+    fi
 
     # .condarc
     echo "channels:" > "${HOME}/.condarc"
@@ -26,18 +28,28 @@ setup_conda_environment() {
     echo "  - defaults" >> "${HOME}/.condarc"
     echo "show_channel_urls: true" >> "${HOME}/.condarc"
 
-    # 将conda的bin目录添加到PATH环境变量，但不影响脚本外部环境
+    # PATH环境变量
     local CONDA_BIN_PATH="/${HOME}/miniconda/bin"
-    echo 'export PATH="$CONDA_BIN_PATH:$PATH"' >> ~/.bashrc
-    echo 'export PATH="$CONDA_BIN_PATH:$PATH"' >> ~/.zshrc
+    if ! grep -q "${CONDA_BIN_PATH}" ~/.bashrc; then
+        echo "export PATH=${CONDA_BIN_PATH}:\$PATH" >> ~/.bashrc
+    fi
+    if ! grep -q "${CONDA_BIN_PATH}" ~/.zshrc; then
+        echo "export PATH=${CONDA_BIN_PATH}:\$PATH" >> ~/.zshrc
+    fi
+    # if [[ ":$PATH:" != *":${CONDA_BIN_PATH}:"* ]]; then
+    #     export PATH="${CONDA_BIN_PATH}:$PATH"
+    # fi
 
     # 安装pytorch、faiss等依赖
-    conda install -y pytorch pytorch-cuda=12.4 python=3.12 faiss-gpu pandas -c pytorch -c nvidia
+    echo "[...]conda installing pytorch, faiss, pandas..."
+    conda install -y pytorch pytorch-cuda python=3.12 pandas
     conda clean -ya
     conda init bash
 
     # 清理临时目录
     rm -rf "$TEMP_DIR"
+
+    echo "[SUC]conda安装完成"
 }
 
 # 调用函数来设置conda环境
