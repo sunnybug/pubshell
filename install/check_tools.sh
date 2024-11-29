00819c34239869fe65ef0c211e3d8833ee9f87ac
xushuwei <xushuwei@x.com>
2024-11-29 13:24:56 +0800

    x

--- install/check_tools.sh
+++ install/check_tools.sh
@@ -44,8 +44,8 @@ check_tools() {
     fi
     
     if [ "$fail" = "y" ]; then
-        echo "是否安装？ ${missing_tools}  [可选]"
-        read -p "请输入 (y/n): " REPLY
+        echo "是否安装？ ${missing_tools}  请输入 (y/n): [可选]"
+        read REPLY
         if [ "$REPLY" = "y" ]; then
             $cmd_prefix apt update && $cmd_prefix apt install ${missing_tools}
             if [ $? -ne 0 ]; then
@@ -56,8 +56,8 @@ check_tools() {
     fi
 
     if [ "$fail_zsh" = "y" ]; then
-        echo "是否安装必需工具？ ${missing_tools_zsh} (y/n)"
-        read -p "请输入 (y/n): " REPLY
+        echo "是否安装必需工具？ ${missing_tools_zsh} 请输入 (y/n): "
+        read REPLY
         if [ "$REPLY" = "y" ]; then
             $cmd_prefix apt update && $cmd_prefix apt install ${missing_tools_zsh}
             if [ $? -ne 0 ]; then
