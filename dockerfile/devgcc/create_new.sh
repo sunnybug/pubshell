0bf7cf0e189d86be47fc513b885c45e8381f6115
xushuwei <xushuwei@x.com>
2024-12-03 11:02:08 +0800

    x

---
+++ dockerfile/devgcc/create_new.sh
@@ -0,0 +1,56 @@
+#!/bin/bash
+
+# 接收所有传递给脚本的参数
+args=("$@")
+
+create_dev_environment() {
+    local dir_name=$1
+
+    if [ -z "$dir_name" ]; then
+        echo "错误：缺少参数，请提供目录(容器）名称。"
+        exit 1
+    fi
+
+    # 创建目录并进入
+    mkdir -p ~/devgcc/$dir_name && cd ~/devgcc
+
+    # 如何cd失败则退出
+    if [ $? -ne 0 ]; then
+        echo "创建目录失败，请检查权限或路径是否正确。"
+        exit 1
+    fi
+
+    # 遍历~/devgcc下所有子目录中的.env,找到最大的PORT_BASE
+    max_port_base=14
+    for env_file in $(find ~/devgcc -type f -name ".env"); do
+        port_base=$(grep -oP 'PORT_BASE=\K\d+' $env_file)
+        if [ ! -z "$port_base" ] && [ "$port_base" -gt "$max_port_base" ]; then
+            max_port_base=$port_base
+        fi
+    done
+
+    # 下载文件
+    curl -o $dir_name/docker-compose.yml https://gitee.com/sunnybug/pubshell/raw/main/dockerfile/devgcc/dev14/docker-compose.yml
+    curl -o $dir_name/.env https://gitee.com/sunnybug/pubshell/raw/main/dockerfile/devgcc/dev14/.env
+    curl -o start.sh https://gitee.com/sunnybug/pubshell/raw/main/dockerfile/devgcc/start.sh
+    curl -o remove.sh https://gitee.com/sunnybug/pubshell/raw/main/dockerfile/devgcc/remove.sh
+    curl -o enter.sh https://gitee.com/sunnybug/pubshell/raw/main/dockerfile/devgcc/enter.sh
+
+    # 使 start.sh 脚本可执行
+    chmod +x ~/devgcc/*.sh
+
+    # 替换目录中所有文件里的字符串 "dev14" 为 $dir_name
+    find . -type f -exec sed -i "s/dev14/$dir_name/g" {} \;
+
+    # 修改$dir_name/.env中的PORT_BASE
+    sed -i "s/PORT_BASE=.*/PORT_BASE=$((max_port_base + 1))/" $dir_name/.env
+
+    echo "创建~/devgcc/$dir_name 成功，启动容器:"
+    echo "cd ~/devgcc && ./start.sh $dir_name"
+
+    source $dir_name/.env
+    echo "SSH_PORT: $SSH_PORT"
+}
+
+# 使用函数，传入你想要的目录名称作为参数
+create_dev_environment "${args[@]}"
--- install/check_tools.sh
+++ install/check_tools.sh
@@ -44,28 +44,17 @@ check_tools() {
     fi
     
     if [ "$fail" = "y" ]; then
-        echo "是否安装？ ${missing_tools}  请输入 (y/n): [可选]"
-        read REPLY < /dev/tty
-        if [ "$REPLY" = "y" ]; then
-            $cmd_prefix apt update && $cmd_prefix apt install ${missing_tools}
-            if [ $? -ne 0 ]; then
-                echo "安装工具失败" >&2
-                exit 1
-            fi
+        $cmd_prefix apt update && $cmd_prefix apt install ${missing_tools} -y
+        if [ $? -ne 0 ]; then
+            echo "安装工具失败" >&2
+            exit 1
         fi
     fi
 
     if [ "$fail_zsh" = "y" ]; then
-        echo "是否安装必需工具？ ${missing_tools_zsh} 请输入 (y/n): "
-        read REPLY < /dev/tty
-        if [ "$REPLY" = "y" ]; then
-            $cmd_prefix apt update && $cmd_prefix apt install ${missing_tools_zsh}
-            if [ $? -ne 0 ]; then
-                echo "安装工具失败" >&2
-                exit 1
-            fi
-        else
-            echo "缺少必需工具，无法使用pubshell" >&2
+        $cmd_prefix apt update && $cmd_prefix apt install ${missing_tools_zsh} -y
+        if [ $? -ne 0 ]; then
+            echo "安装工具失败" >&2
             exit 1
         fi
     fi
